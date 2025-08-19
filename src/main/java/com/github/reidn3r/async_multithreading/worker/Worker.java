5b2ed543eed5df78496ffb45a562e24ac08d0d7a
reidn3r <rdn.adn00@gmail.com>
2025-08-18 21:12:23 -0300

    feat: worker persiste em disco

--- src/main/java/com/github/reidn3r/async_multithreading/worker/Worker.java
+++ src/main/java/com/github/reidn3r/async_multithreading/worker/Worker.java
@@ -4,14 +4,19 @@ import java.util.Map;
 import java.util.concurrent.Executor;
 
 import org.springframework.stereotype.Component;
+
+import com.github.reidn3r.async_multithreading.services.DbService;
+
 import io.lettuce.core.StreamMessage;
 
 @Component
 public class Worker {
   private final Executor threadExecutor;
+  private final DbService pgsql;
 
-  public Worker(Executor threadExecutor){
+  public Worker(Executor threadExecutor, DbService db) {
     this.threadExecutor = threadExecutor;
+    this.pgsql = db;
   }
 
   public void process(StreamMessage<String, String> message){
@@ -22,9 +27,7 @@ public class Worker {
       Long userId = Long.valueOf(body.get("userId"));
       String interaction = body.get("interaction");
 
-      System.out.println("Virtual Thread: OK!");
-      
-      // Processamento em virtual thread
+      pgsql.save(userId, postId, interaction);      
     });
   }
   }
